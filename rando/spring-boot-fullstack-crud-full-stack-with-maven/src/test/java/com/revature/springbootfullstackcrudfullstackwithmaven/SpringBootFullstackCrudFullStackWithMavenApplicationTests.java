b6b91562fb6bc0c4f0b38d27973f8699f5824a70
asmithjr1989 <amsithjr1989>
2020-03-29 20:02:16 -0500

    backed

---
+++ rando/spring-boot-fullstack-crud-full-stack-with-maven/src/test/java/com/revature/springbootfullstackcrudfullstackwithmaven/SpringBootFullstackCrudFullStackWithMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.springbootfullstackcrudfullstackwithmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullstackCrudFullStackWithMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
