c410593d6cb021797aa0b8881b18c9b0eafdb8da
Jared Carter <jaredallencarterjac@gmail.com>
2020-03-29 17:31:00 -0700

    rando spring

---
+++ spring-boot-fullstack-crud-full-stack-with-maven/src/main/java/com/revature/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/RandoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-fullstack-crud-full-stack-with-maven/src/test/java/com/revature/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/RandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
